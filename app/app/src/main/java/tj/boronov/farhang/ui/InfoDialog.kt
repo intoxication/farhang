cacab9e4cbb7cc08235a1a1acaad3a702128cf89
boronov <fsociety.faridun@gmail.com>
2021-01-18 23:19:51 +0500

    add: rate app button into InfoDialog

--- app/app/src/main/java/tj/boronov/farhang/ui/InfoDialog.kt
+++ app/app/src/main/java/tj/boronov/farhang/ui/InfoDialog.kt
@@ -1,19 +1,48 @@
 package tj.boronov.farhang.ui
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import tj.boronov.farhang.R
+import tj.boronov.farhang.databinding.DialogInfoBinding
 
 class InfoDialog : DialogFragment() {
+
+    lateinit var binding: DialogInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.dialog_info, container, false)
+        binding = DialogInfoBinding.inflate(layoutInflater, container, false)
+
+        // Rate app button
+        binding.rateApp.setOnClickListener {
+            val uri: Uri = Uri.parse("market://details?id=tj.boronov.farhang")
+            val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+            goToMarket.addFlags(
+                Intent.FLAG_ACTIVITY_NO_HISTORY or
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+            )
+            try {
+                startActivity(goToMarket)
+            } catch (e: ActivityNotFoundException) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=tj.boronov.farhang")
+                    )
+                )
+            }
+        }
+
+        return binding.root
     }
 }
