a84a42b39fd2a0ef60c8e1eccd0a4bc32351ca5b
boronov <fsociety.faridun@gmail.com>
2021-01-25 11:45:39 +0500

    add: firebase analytics

--- app/app/src/main/java/tj/boronov/farhang/App.kt
+++ app/app/src/main/java/tj/boronov/farhang/App.kt
@@ -2,12 +2,17 @@ package tj.boronov.farhang
 
 import android.app.Application
 import androidx.room.Room
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import tj.boronov.farhang.data.FarhangDatabase
 import tj.boronov.farhang.util.*
 
 class App : Application() {
+
     override fun onCreate() {
         super.onCreate()
+        mFirebaseAnalytics = Firebase.analytics
 
         database = Room.databaseBuilder(this, FarhangDatabase::class.java, DATABASE_NAME)
             .createFromAsset("database/data.db")
@@ -17,5 +22,6 @@ class App : Application() {
 
     companion object {
         lateinit var database: FarhangDatabase private set
+        private lateinit var mFirebaseAnalytics: FirebaseAnalytics
     }
 }
